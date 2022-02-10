0e994428de7d5f8cf4b3b08dab31e8c22b3875b9
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2022-02-09 22:59:40 -0500

    Initial commit

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+from user import User
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def create():
+    # call the get_all classmethod to get all users
+    users = User.get_all()
+    print(users)
+    return render_template('read.html', all_users = users)
+
+@app.route('/add_user')
+def add_user():
+    return render_template('create.html')
+
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"]
+    }
+
+    User.save(data)
+
+    return redirect('/')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ user.py
@@ -0,0 +1,36 @@
+# import the function that will return the instance of a connection
+from mysqlconnection import connectToMySQL
+
+#model the class after the user table  from our databse 
+
+class User:
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name= data['first_name']
+        self.last_name= data['last_name']
+        self.email= data['email']
+        self.created_at= data['created_at']
+        self.updated_at= data['updated_at']
+
+
+    # Use class methods to query the database 
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        # call the connectToMySQL function with the schema you are targeting 
+
+        users_db = connectToMySQL('users').query_db(query)
+
+        # create an empty list to append our instances of users
+        users = []
+        # iterate over the db results and create instances of users with cls 
+        for user in users_db:
+            users.append(cls(user))
+        return users
+
+    # class method to save a user to the database
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(fname)s,%(lname)s, %(email)s, now(), now() );"
+
+        return connectToMySQL('users').query_db(query, data)
